6037268ce65ea6f185a6cce7a72d5c6c67115b24
Olha <ololoevaevaeva@gmail.com>
2019-11-06 13:37:26 +0200

    code project

--- js/common.js
+++ js/common.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
         $(".header-right, .overlay").addClass('active');
         $.scrollLock(true);
     });
-    $(".close").click(function () {
+    $(".close, .overlay").click(function () {
         $(".header-right, .overlay").removeClass('active');
         $.scrollLock(false);
     });
@@ -40,49 +40,4 @@ $(document).ready(function () {
     }
     $(".footer-top").scrollToTop();
 
-    $('.header-lang').each(function(){
-        var $this = $(this), numberOfOptions = $(this).children('option').length;
-
-        $this.addClass('select-hidden');
-        $this.wrap('<div class="select"></div>');
-        $this.after('<div class="select-styled"></div>');
-
-        var $styledSelect = $this.next('div.select-styled');
-        $styledSelect.text($this.children('option').eq(0).text());
-
-        var $list = $('<ul />', {
-            'class': 'select-options'
-        }).insertAfter($styledSelect);
-
-        for (var i = 0; i < numberOfOptions; i++) {
-            $('<li />', {
-                text: $this.children('option').eq(i).text(),
-                rel: $this.children('option').eq(i).val()
-            }).appendTo($list);
-        }
-
-        var $listItems = $list.children('li');
-
-        $styledSelect.click(function(e) {
-            e.stopPropagation();
-            $('div.select-styled.active').not(this).each(function(){
-                $(this).removeClass('active').next('ul.select-options').hide();
-            });
-            $(this).toggleClass('active').next('ul.select-options').toggle();
-        });
-
-        $listItems.click(function(e) {
-            e.stopPropagation();
-            $styledSelect.text($(this).text()).removeClass('active');
-            $this.val($(this).attr('rel'));
-            $list.hide();
-            //console.log($this.val());
-        });
-
-        $(document).click(function() {
-            $styledSelect.removeClass('active');
-            $list.hide();
-        });
-
-    });
 });
